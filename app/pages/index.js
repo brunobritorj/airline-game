e7fa93da87aa99f03f270ca9390f4d5a4ea7dd62
Bruno Silva <brunobritorj@outlook.com>
2023-08-02 21:02:33 +0200

    Reducing to the minimal

--- app/pages/index.js
+++ app/pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
     <>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello World!</h1>
     </>
