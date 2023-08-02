96a39fc4ec64f614ed720d56b5966dd695a11f7a
Bruno Silva <brunobritorj@outlook.com>
2023-08-02 22:52:28 +0200

    NextAuth working

---
+++ app/components/Layout-old.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.svg"></link>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossOrigin="anonymous"
+      />
+      <div className="container">
+        {children}
+      </div>
+    </>
+  );
+};
+
+Layout.defaultProps = {
+  title: 'Airline Game'
+};
+
+export default Layout;
--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import Head from 'next/head';
-import Script from 'next/script';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
-const Layout = ({ title, children }) => {
+export default function Layout({ title, children }) {
+  const { data: session } = useSession();
+
   return (
-    <>
-      <Head>
-        <title>{title}</title>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossOrigin="anonymous"
-      />
-      <div className="container">
+    <div>
+      <header>
+        <nav>
+          {!session ? (
+            <button onClick={() => signIn('azure-ad')}>Sign in</button>
+          ) : (
+            <div>
+              <p>Welcome, {session.user.email}!</p>
+              <button onClick={() => signOut()}>Sign out</button>
+            </div>
+          )}
+        </nav>
+      </header>
+      <main>
+        <h1>{title}</h1>
         {children}
-      </div>
-    </>
+      </main>
+      <footer>...</footer>
+    </div>
   );
-};
-
-Layout.defaultProps = {
-  title: 'Airline Game'
-};
-
-export default Layout;
+}
