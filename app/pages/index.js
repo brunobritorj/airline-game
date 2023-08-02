dfc978e8677f9e3a899a5809e0e86343daedfa53
Bruno Silva <brunobritorj@outlook.com>
2023-08-02 21:18:01 +0200

    Introducing Layout/Template

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,19 +1,9 @@
-import Head from 'next/head';
-import Script from "next/script";
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Minimal NextJS App</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
-      </Head>
-      <Script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-        crossorigin="anonymous"
-      />
+    <Layout>
       <h1>Hello World!</h1>
-    </>
-  )
+    </Layout>
+  );
 }
