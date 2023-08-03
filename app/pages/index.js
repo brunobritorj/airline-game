cf76104ac789f712901b71b1e3bb8258a1c59c03
Bruno Silva <brunobritorj@outlook.com>
2023-08-03 23:51:14 +0200

    General changes

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,13 +1,17 @@
 import { useSession } from 'next-auth/react';
 import Layout from '../components/Layout';
+import LayoutLogin from '../components/LayoutLogin';
 
 export default function Home() {
   const { data: session } = useSession();
 
-  return (
-    <Layout title="Airline Game | Home">
-      <h1>Hello World!</h1>
-      {session && <p>Welcome, {session.user.email}!</p>}
-    </Layout>
-  );
+  if (!session) {
+    return <Layout />;
+  } else {
+    return <Layout title="Airline Game | Home">
+        <h1>Hello World!</h1>
+        {session && <p>Welcome, {session.user.email}!</p>}
+      </Layout>;
+
+  }
 }
