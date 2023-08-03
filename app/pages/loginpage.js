ba6b577d783de5dbb5935d5bab4a95ad539292ef
Bruno Silva <brunobritorj@outlook.com>
2023-08-04 00:01:24 +0200

    Testing a new login page

--- app/pages/loginpage.js
+++ app/pages/loginpage.js
@@ -1,11 +1,12 @@
 import { useSession } from 'next-auth/react';
 import Layout from '../components/Layout';
+import LayoutLogin from '../components/LayoutLogin';
 
 export default function Home() {
   const { data: session } = useSession();
 
   if (!session) {
-    return <Layout />;
+    return <LayoutLogin />;
   } else {
     return <Layout title="Airline Game | Home">
         <h1>Hello World!</h1>
