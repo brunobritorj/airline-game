38b31aa8a472609513d349ad6af823925cd75cb8
Bruno Silva <brunobritorj@outlook.com>
2023-08-05 17:04:15 +0200

    New visual

--- app/components/div/LayoutHeader.js
+++ app/components/div/LayoutHeader.js
@@ -7,25 +7,25 @@ export default function LayoutHeader({}){
           <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <ul className="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
               <li>
-                <Link href="/feed" className="nav-link text-white">
+                <Link href="/feed" className="nav-link text-white centered-link">
                   <img className="bi d-block mx-auto mb-1" src="/images/feed-white-icon.svg" width="48" height="38" alt="My SVG" />
                   Feed
                 </Link>
               </li>
               <li>
-                <Link href="aircrafts" className="nav-link text-white">
+                <Link href="aircrafts" className="nav-link text-white centered-link">
                   <img className="bi d-block mx-auto mb-1" src="/images/aircrafts-white-icon.svg" width="48" height="38" alt="My SVG" />
                   Aeronaves
                 </Link>
               </li>
               <li>
-                <Link href="airports" className="nav-link text-white">
+                <Link href="airports" className="nav-link text-white centered-link">
                   <img className="bi d-block mx-auto mb-1" src="/images/airports-white-icon.svg" width="48" height="38" alt="My SVG" />
                   Aeroportos
                 </Link>
               </li>
               <li>
-                <Link href="routes" className="nav-link text-white">
+                <Link href="routes" className="nav-link text-white centered-link">
                   <img className="bi d-block mx-auto mb-1" src="/images/routes-white-icon.svg" width="48" height="38" alt="My SVG" />
                   Rotas
                 </Link>
--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,6 +1,7 @@
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 import '../styles/offcanvas.css';
+import '../styles/custom.css';
 
 function MyApp({ Component, pageProps }) {
   return (
